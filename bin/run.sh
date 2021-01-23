bd4d467bcabfe8b8695ec2d7ac5476855c633b59
teagueStockwell <tsAppDevelopment@gmail.com>
2021-01-22 19:12:42 -0800

    composing container and stuff

--- bin/run.sh
+++ bin/run.sh
@@ -2,10 +2,10 @@
 docker-compose down
 export $(cat .env | xargs)
 sed 's/${APP}/'"$APP"'/g' ./src/nginx.conf > ./src/nginx.generated.conf
-sed -i 's/${FRONTEND_PORT}/'"$FRONTEND_PORT"'/g' ./src/nginx.generated.conf
-sed -i 's/${BACKEND_PORT}/'"$BACKEND_PORT"'/g' ./src/nginx.generated.conf
+sed -i.bak 's/${FRONTEND_PORT}/'"$FRONTEND_PORT"'/g' ./src/nginx.generated.conf
+sed -i.bak 's/${BACKEND_PORT}/'"$BACKEND_PORT"'/g' ./src/nginx.generated.conf
 JWT=$(base64 -w 0 ./src/person.json)
-sed -i 's/${JWT}/'"${JWT}"'/g' ./src/nginx.generated.conf
+sed -i.bak 's/${JWT}/'"${JWT}"'/g' ./src/nginx.generated.conf
 if [ -n "$BACKEND_FOLDER" ];
 then
     cd $BACKEND_FOLDER
